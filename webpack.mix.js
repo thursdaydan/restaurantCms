00e9db6d94f909e73769e04ff2c465eabed516d2
thursdaydan <hulldeveloper@gmail.com>
2020-12-31 12:33:01 +0000

    Updated npm + composer packages.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,23 +1,15 @@
 const mix = require('laravel-mix');
-const domain = 'restaurantcms.localhost';
-const homedir = require('os').homedir();
 
-mix.browserSync({
-    proxy: 'https://' + domain,
-    browser: 'google chrome',
-    host: domain,
-    open: 'external',
-    https: {
-        key: homedir + '/.config/valet/Certificates/' + domain + '.key',
-        cert: homedir + '/.config/valet/Certificates/' + domain + '.crt'
-    },
-});
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
-
-if (mix.inProduction()) {
-    mix.version()
-        .disableNotifications();
-}
+    .sass('resources/sass/app.scss', 'public/css');
